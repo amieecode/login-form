0d2dff93a96fdd25122012b42df650cb1d2399a0
amieecode <enwelumchiamaka@gmail.com>
2024-07-10 19:46:41 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+     <h2 className='text-center font-bold'>LOGIN</h2>
+    </>
+  )
+}
+
+export default App;
