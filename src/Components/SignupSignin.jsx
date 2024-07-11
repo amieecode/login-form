1412f34345bd8c8d9ce453034f4aea9d05ce17d9
amieecode <enwelumchiamaka@gmail.com>
2024-07-11 02:29:50 +0100

    login with social

--- src/Components/SignupSignin.jsx
+++ src/Components/SignupSignin.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
 import { UserRound, Mail, Lock } from 'lucide-react';
+import FImage from "../assets/Facebook.png";
+import GImage from "../assets/Google.png";
+import LImage from "../assets/LinkedIn.png";
 
 const SignupSignin = () => {
     const[action, setAction] = useState("Sign In")
   return (
     <>
-      <div className='flex flex-col m-auto mt-52 pb-8 bg-white w-[600px]'>
+      <div className='flex flex-col m-auto mt-40 pb-8 bg-white w-[600px]'>
         <div className='flex flex-col items-center gap-2 w-full mt-8'>
-            <div className='font-bold text-[#3c009d] text-[48px]'>{action}</div>
+            <div className='font-bold text-[#3c009d] text-[48px]'>{action}</div>{/*Title*/}
             {/*Underline*/}
             <div className='w-16 h-[6px] bg-[#3c009d] rounded-lg'></div>
         </div>
 
-        <div className='flex flex-col mt-14 gap-6'>
-            <div className='flex items-center m-auto w-[480px] h-20 bg-[#eaeaea] rounded'>
+        <div className='flex flex-col flex-wrap mt-14 gap-6'>
+          {action === "Sign In" ?<div></div>: <div className='flex items-center m-auto w-[480px] h-20 bg-[#eaeaea] rounded'>
                 <UserRound className='mx-8 my-0'/>
                 <input 
                     type="text" 
                     placeholder='Name'
                     className='h-[50px] w-[400px] bg-transparent border-none outline-none text-[#797979] text-[19px]' 
                 />
-            </div>
+            </div>}{/*Ternary operator to hide username for Sign in process*/}
+            
             <div className='flex items-center m-auto w-[480px] h-20 bg-[#eaeaea] rounded'>
                 <Mail className='mx-8 my-0' />
                 <input 
@@ -37,14 +41,35 @@ const SignupSignin = () => {
                     className='h-[50px] w-[400px] bg-transparent border-none outline-none text-[#797979] text-[19px]' 
                 />
             </div>
-            <div className='pl-16 mt-7 text-[#797979] text-lg'>
-                Lost PassWord ? <span className='text-[#4c00b4] cursor-pointer'>Click Here !</span>
-            </div>
-            <div className='flex gap-8 mx-auto my-16'>
+
+            {action === "Sign Up" ? <div></div> : <div className='flex justify-center gap-20 text-[#797979] text-lg'>
+                <div className=''><input type="checkbox" className='w-4 h-4 gap-2' /> Remember me</div>
+                <div className='flex'> Forgot Password? <span className='text-[#4c00b4] cursor-pointer'>Click Here !</span></div>
+            </div> }{/*Ternary operator to hide forgot password from sign up process*/}
+        </div>   
+            <div className='flex flex-wrap gap-8 mx-auto my-8'>
                 <div className={action === "Sign In" ? "submit gray" : "submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action === "Sign Up" ? "submit gray" : "Submit"} onClick={()=>{setAction("Sign In")}}>Sign In</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={()=>{setAction("Sign In")}}>Sign In</div>
             </div>
-        </div>
+
+            <div className='flex justify-center items-center text-lg text-[#797979] '>Or login with</div>
+            {/*Social Login*/}
+            <div className='flex flex-wrap gap-10 m-auto mt-8'>
+                <div className='social'>
+                    <img src={GImage} alt="Google" className='w-12 h-12 mx-10 mt-0.5' />
+                </div>
+                <div className='social'>
+                    <img src={FImage} alt="Facebook" className='w-10 h-10 mx-10 mt-2'  />
+                </div>
+                <div className='social'>
+                    <img src={LImage} alt="Google" className='w-12 h-12 mx-10 mt-1.5'  />
+                </div>
+            </div>
+
+            <div className='flex justify-center pl-4 mx-auto my-8 text-[#797979] gap-1'>
+                I don't have an account? <span className='text-[#4c00b4] cursor-pointer'> Register</span>
+            </div>
+        
       </div>
 
     </>
